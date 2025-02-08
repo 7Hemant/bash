59109105a59170935db9952702f229f1323eab6b
Hemant <hemantmourya127@gmail.com>
2025-02-08 22:53:13 +0530

    hello world and  variable

---
+++ helloworld.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Hello Bash"
+
+echo "This is first time, I'm exploring bash scripting: "$SHELL
---
+++ variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+name="hemant"
+age=25
+
+echo "Hello there, I'm Hemant and my age is $age."
